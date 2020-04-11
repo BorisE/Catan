f016588397047818df278e65a04b4d9ce8b69d84
Emchenko <Emchenko@MAIN>
2020-04-11 11:21:52 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.field1 = new System.Windows.Forms.Button();
+            this.field2 = new System.Windows.Forms.Button();
+            this.field3 = new System.Windows.Forms.Button();
+            this.field4 = new System.Windows.Forms.Button();
+            this.field5 = new System.Windows.Forms.Button();
+            this.field11 = new System.Windows.Forms.Button();
+            this.field12 = new System.Windows.Forms.Button();
+            this.field13 = new System.Windows.Forms.Button();
+            this.field14 = new System.Windows.Forms.Button();
+            this.field18 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.field15 = new System.Windows.Forms.Button();
+            this.field6 = new System.Windows.Forms.Button();
+            this.field10 = new System.Windows.Forms.Button();
+            this.field17 = new System.Windows.Forms.Button();
+            this.field16 = new System.Windows.Forms.Button();
+            this.field9 = new System.Windows.Forms.Button();
+            this.field8 = new System.Windows.Forms.Button();
+            this.field7 = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // field1
+            // 
+            this.field1.Enabled = false;
+            this.field1.Location = new System.Drawing.Point(176, 33);
+            this.field1.Name = "field1";
+            this.field1.Size = new System.Drawing.Size(130, 130);
+            this.field1.TabIndex = 0;
+            this.field1.UseVisualStyleBackColor = true;
+            // 
+            // field2
+            // 
+            this.field2.Enabled = false;
+            this.field2.Location = new System.Drawing.Point(312, 33);
+            this.field2.Name = "field2";
+            this.field2.Size = new System.Drawing.Size(130, 130);
+            this.field2.TabIndex = 0;
+            this.field2.UseVisualStyleBackColor = true;
+            // 
+            // field3
+            // 
+            this.field3.Enabled = false;
+            this.field3.Location = new System.Drawing.Point(448, 33);
+            this.field3.Name = "field3";
+            this.field3.Size = new System.Drawing.Size(130, 130);
+            this.field3.TabIndex = 0;
+            this.field3.UseVisualStyleBackColor = true;
+            // 
+            // field4
+            // 
+            this.field4.Enabled = false;
+            this.field4.Location = new System.Drawing.Point(512, 169);
+            this.field4.Name = "field4";
+            this.field4.Size = new System.Drawing.Size(130, 130);
+            this.field4.TabIndex = 0;
+            this.field4.UseVisualStyleBackColor = true;
+            // 
+            // field5
+            // 
+            this.field5.Enabled = false;
+            this.field5.Location = new System.Drawing.Point(577, 305);
+            this.field5.Name = "field5";
+            this.field5.Size = new System.Drawing.Size(130, 130);
+            this.field5.TabIndex = 0;
+            this.field5.UseVisualStyleBackColor = true;
+            // 
+            // field11
+            // 
+            this.field11.Enabled = false;
+            this.field11.Location = new System.Drawing.Point(33, 305);
+            this.field11.Name = "field11";
+            this.field11.Size = new System.Drawing.Size(130, 130);
+            this.field11.TabIndex = 0;
+            this.field11.UseVisualStyleBackColor = true;
+            // 
+            // field12
+            // 
+            this.field12.Enabled = false;
+            this.field12.Location = new System.Drawing.Point(104, 169);
+            this.field12.Name = "field12";
+            this.field12.Size = new System.Drawing.Size(130, 130);
+            this.field12.TabIndex = 0;
+            this.field12.UseVisualStyleBackColor = true;
+            // 
+            // field13
+            // 
+            this.field13.Enabled = false;
+            this.field13.Location = new System.Drawing.Point(240, 169);
+            this.field13.Name = "field13";
+            this.field13.Size = new System.Drawing.Size(130, 130);
+            this.field13.TabIndex = 0;
+            this.field13.UseVisualStyleBackColor = true;
+            // 
+            // field14
+            // 
+            this.field14.Enabled = false;
+            this.field14.Location = new System.Drawing.Point(376, 169);
+            this.field14.Name = "field14";
+            this.field14.Size = new System.Drawing.Size(130, 130);
+            this.field14.TabIndex = 0;
+            this.field14.UseVisualStyleBackColor = true;
+            // 
+            // field18
+            // 
+            this.field18.Enabled = false;
+            this.field18.Location = new System.Drawing.Point(169, 305);
+            this.field18.Name = "field18";
+            this.field18.Size = new System.Drawing.Size(130, 130);
+            this.field18.TabIndex = 0;
+            this.field18.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.DarkOrange;
+            this.button15.Enabled = false;
+            this.button15.Location = new System.Drawing.Point(305, 305);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(130, 130);
+            this.button15.TabIndex = 0;
+            this.button15.UseVisualStyleBackColor = false;
+            // 
+            // field15
+            // 
+            this.field15.Enabled = false;
+            this.field15.Location = new System.Drawing.Point(441, 305);
+            this.field15.Name = "field15";
+            this.field15.Size = new System.Drawing.Size(130, 130);
+            this.field15.TabIndex = 0;
+            this.field15.UseVisualStyleBackColor = true;
+            // 
+            // field6
+            // 
+            this.field6.Enabled = false;
+            this.field6.Location = new System.Drawing.Point(512, 441);
+            this.field6.Name = "field6";
+            this.field6.Size = new System.Drawing.Size(130, 130);
+            this.field6.TabIndex = 0;
+            this.field6.UseVisualStyleBackColor = true;
+            // 
+            // field10
+            // 
+            this.field10.Enabled = false;
+            this.field10.Location = new System.Drawing.Point(104, 441);
+            this.field10.Name = "field10";
+            this.field10.Size = new System.Drawing.Size(130, 130);
+            this.field10.TabIndex = 0;
+            this.field10.UseVisualStyleBackColor = true;
+            // 
+            // field17
+            // 
+            this.field17.Enabled = false;
+            this.field17.Location = new System.Drawing.Point(240, 441);
+            this.field17.Name = "field17";
+            this.field17.Size = new System.Drawing.Size(130, 130);
+            this.field17.TabIndex = 0;
+            this.field17.UseVisualStyleBackColor = true;
+            // 
+            // field16
+            // 
+            this.field16.Enabled = false;
+            this.field16.Location = new System.Drawing.Point(376, 441);
+            this.field16.Name = "field16";
+            this.field16.Size = new System.Drawing.Size(130, 130);
+            this.field16.TabIndex = 0;
+            this.field16.UseVisualStyleBackColor = true;
+            // 
+            // field9
+            // 
+            this.field9.Enabled = false;
+            this.field9.Location = new System.Drawing.Point(176, 577);
+            this.field9.Name = "field9";
+            this.field9.Size = new System.Drawing.Size(130, 130);
+            this.field9.TabIndex = 0;
+            this.field9.UseVisualStyleBackColor = true;
+            // 
+            // field8
+            // 
+            this.field8.Enabled = false;
+            this.field8.Location = new System.Drawing.Point(312, 577);
+            this.field8.Name = "field8";
+            this.field8.Size = new System.Drawing.Size(130, 130);
+            this.field8.TabIndex = 0;
+            this.field8.UseVisualStyleBackColor = true;
+            // 
+            // field7
+            // 
+            this.field7.Enabled = false;
+            this.field7.Location = new System.Drawing.Point(448, 577);
+            this.field7.Name = "field7";
+            this.field7.Size = new System.Drawing.Size(130, 130);
+            this.field7.TabIndex = 0;
+            this.field7.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(801, 33);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(220, 89);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 740);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.field16);
+            this.Controls.Add(this.field14);
+            this.Controls.Add(this.field17);
+            this.Controls.Add(this.field13);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.field18);
+            this.Controls.Add(this.field10);
+            this.Controls.Add(this.field12);
+            this.Controls.Add(this.field11);
+            this.Controls.Add(this.field15);
+            this.Controls.Add(this.field5);
+            this.Controls.Add(this.field6);
+            this.Controls.Add(this.field4);
+            this.Controls.Add(this.field7);
+            this.Controls.Add(this.field8);
+            this.Controls.Add(this.field3);
+            this.Controls.Add(this.field9);
+            this.Controls.Add(this.field2);
+            this.Controls.Add(this.field1);
+            this.Name = "MainForm";
+            this.Text = "CATAN";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button field1;
+        private System.Windows.Forms.Button field2;
+        private System.Windows.Forms.Button field3;
+        private System.Windows.Forms.Button field4;
+        private System.Windows.Forms.Button field5;
+        private System.Windows.Forms.Button field11;
+        private System.Windows.Forms.Button field12;
+        private System.Windows.Forms.Button field13;
+        private System.Windows.Forms.Button field14;
+        private System.Windows.Forms.Button field18;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button field15;
+        private System.Windows.Forms.Button field6;
+        private System.Windows.Forms.Button field10;
+        private System.Windows.Forms.Button field17;
+        private System.Windows.Forms.Button field16;
+        private System.Windows.Forms.Button field9;
+        private System.Windows.Forms.Button field8;
+        private System.Windows.Forms.Button field7;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+     public partial class MainForm : Form
+    {
+        public enum FieldType
+        {
+            None = -1,
+            Desert = 0,
+            Pasture = 1,
+            Field = 2,
+            Forest = 3,
+            Mountain = 4,
+            Hill = 5
+
+        }
+
+        public class CatanFieldElement
+        {
+            public string Name = "";
+            public int id = 0;
+            public int Num = 0;
+            public FieldType Type = FieldType.None;
+        }
+
+        CatanFieldElement[] CatanField = new CatanFieldElement[18];
+
+        public class CatanFieldClass
+        {
+            public CatanFieldElement[] FieldItems = new CatanFieldElement[18];
+
+
+        }
+
+
+        List<FieldType> AvailableFields;
+        List<int> AvailableNums;
+
+        public class CoorPair
+        {
+            public int x;
+            public int y;
+
+            public CoorPair(int extx, int exty)
+            {
+                x = extx;
+                y = exty;
+            }
+        }
+
+        Dictionary<int, Tuple<int, int>> Coordinates_old = new Dictionary<int, Tuple<int, int>>();
+        Dictionary<int, CoorPair> Coordinates = new Dictionary<int, CoorPair>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            InitFields();
+
+            GenerateField();
+
+            DrawPannels();
+        }
+
+        void InitFields()
+        {
+            AvailableFields = new List<FieldType>()
+            {
+                FieldType.Pasture, FieldType.Pasture, FieldType.Pasture, FieldType.Pasture,
+                FieldType.Field, FieldType.Field, FieldType.Field, FieldType.Field,
+                FieldType.Forest, FieldType.Forest, FieldType.Forest, FieldType.Forest,
+                FieldType.Mountain, FieldType.Mountain, FieldType.Mountain,
+                FieldType.Hill,FieldType.Hill,FieldType.Hill
+            };
+
+            AvailableNums = new List<int>()
+            {
+                1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6,
+                8, 8, 9, 9, 10, 10, 11, 11, 12
+            };
+
+
+            Coordinates_old.Add(0, new Tuple<int, int>(2, 0));
+            Coordinates_old.Add(1, new Tuple<int, int>(4, 0));
+            Coordinates_old.Add(2, new Tuple<int, int>(6, 0));
+
+            Coordinates.Add(0,  new CoorPair(2, 0));
+            Coordinates.Add(1,  new CoorPair(4, 0));
+            Coordinates.Add(2,  new CoorPair(6, 0));
+
+            Coordinates.Add(11, new CoorPair(1, 2));
+            Coordinates.Add(12, new CoorPair(3, 2));
+            Coordinates.Add(13, new CoorPair(5, 2));
+            Coordinates.Add(3,  new CoorPair(7, 2));
+
+            Coordinates.Add(10, new CoorPair(0, 4));
+            Coordinates.Add(17, new CoorPair(2, 4));
+            Coordinates.Add(18, new CoorPair(4, 4));
+            Coordinates.Add(14, new CoorPair(6, 4));
+            Coordinates.Add(4,  new CoorPair(8, 4));
+
+            Coordinates.Add(9,  new CoorPair(1, 6));
+            Coordinates.Add(16, new CoorPair(3, 6));
+            Coordinates.Add(15, new CoorPair(5, 6));
+            Coordinates.Add(5,  new CoorPair(7, 6));
+
+            Coordinates.Add(8,  new CoorPair(2, 8));
+            Coordinates.Add(7,  new CoorPair(4, 8));
+            Coordinates.Add(6,  new CoorPair(6, 8));
+
+        }
+
+        void GenerateField()
+        {
+            var fldidx = 0;
+            FieldType curField = FieldType.None;
+            Random Rnd = new Random();
+
+            var numidx = 0;
+            int curNum = 0;
+
+            for (int i = 0; i < 18; i++)
+            {
+                fldidx = Rnd.Next(0, AvailableFields.Count - 1);
+                curField = AvailableFields[fldidx];
+                AvailableFields.RemoveAt(fldidx);
+
+                numidx = Rnd.Next(0, AvailableNums.Count - 1);
+                curNum = AvailableNums[numidx];
+                AvailableNums.RemoveAt(numidx);
+
+                CatanField[i] = new CatanFieldElement
+                {
+                    id = i,
+                    Num = curNum,
+                    Type = curField
+                };
+            }
+        }
+
+        private void DrawPannels()
+        {
+            for (int i = 0; i < 18; i++)
+            {
+                this.Controls["field"+(i+1).ToString()].Text = CatanField[i].Num.ToString() + Environment.NewLine + CatanField[i].Type.ToString();
+                this.Controls["field" + (i + 1).ToString()].BackColor = getFieldColor (CatanField[i].Type);
+            }
+        }
+
+        
+        private Color getFieldColor(FieldType Fld)
+        {
+            Color Res;
+
+            if (Fld == FieldType.Desert)
+            {
+                Res = Color.LightYellow;
+            }
+            else if (Fld == FieldType.Pasture)
+            {
+                Res = Color.LightGreen;
+            }
+            else if (Fld == FieldType.Field)
+            {
+                Res = Color.Yellow;
+            }
+            else if (Fld == FieldType.Forest)
+            {
+                Res = Color.ForestGreen;
+            }
+            else if (Fld == FieldType.Mountain)
+            {
+                Res = Color.Silver;
+            }
+            else if (Fld == FieldType.Hill)
+            {
+                Res = Color.Brown;
+            }
+            else
+            {
+                Res = Color.Gray;
+            }
+
+            return Res;
+        }
+
+        private double CalcDistance (CoorPair P1, CoorPair P2)
+        {
+            double dist = Math.Sqrt((P2.x - P1.x) * (P2.x - P1.x) + (P2.y - P1.y) * (P2.y - P1.y));
+            return dist;
+        }
+    }
+}
